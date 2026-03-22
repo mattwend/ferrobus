d381cfe133a7caef5b8cf1a61997aa28aac3ced2
Matthias Wende <code@matthiaswende.de>
2026-03-22 19:15:45 +0000

    chore: add license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 tinymb contributors
+
 use std::error::Error;
 use tracing::info;
 use tracing_subscriber::{filter::EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
