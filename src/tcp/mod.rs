6b7a908ccda63983ab37fc7a3da97a2b2819ab21
Matthias Wende <code@matthiaswende.de>
2026-04-05 10:06:01 +0200

    feat(tcp): allow configurable transport timeouts

--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -5,4 +5,4 @@ pub mod adu;
 pub use adu::build_modbus_tcp_adu;
 
 pub mod tcp_connection;
-pub use tcp_connection::ModbusTcpConnection;
+pub use tcp_connection::{ModbusTcpConnection, ModbusTcpTimeouts};
